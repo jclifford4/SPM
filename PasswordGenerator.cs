5b2308a6b18c2e9e228c988166406051fe199c0d
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2024-05-06 16:59:38 -0700

    Initial commit

---
+++ PasswordGenerator.cs
@@ -0,0 +1,21 @@
+
+namespace PasswordUtility
+{
+    public static class PasswordGenerator
+    {
+        public static string Generate(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghi" +
+                                "jklmnopqrstuvwxyz0123456789!?_()-+*/";
+            var random = new Random();
+            var passwardChars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                passwardChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(passwardChars);
+        }
+    }
+
+}
