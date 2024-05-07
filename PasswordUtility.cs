6ff85195d69ec3abcdb489516cd5f70249c310a0
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2024-05-06 20:15:46 -0700

    Typo

--- PasswordUtility.cs
+++ PasswordUtility.cs
@@ -12,13 +12,13 @@ namespace PasswordUtility
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghi" +
                                 "jklmnopqrstuvwxyz0123456789!?_()-+*/";
             var random = new Random();
-            var passwardChars = new char[length];
+            var passwordChars = new char[length];
             for (int i = 0; i < length; i++)
             {
-                passwardChars[i] = chars[random.Next(chars.Length)];
+                passwordChars[i] = chars[random.Next(chars.Length)];
             }
 
-            return new string(passwardChars);
+            return new string(passwordChars);
         }
     }
 
